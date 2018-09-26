2da86dcbcf3d9ca844aa727ac8c6a72684fe22e5
Liubomyr Petrivnyk <lubomyr.petrivnyk@gmail.com>
2018-09-26 20:17:44 +0300

    Done page home and store..

--- src/js/partials/slider.js
+++ src/js/partials/slider.js
@@ -1,26 +1,44 @@
 $('.header-page').slick({
-    infinite: true,
-    // arrows: false,
-    autoplay: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  });
+  infinite: true,
+  // arrows: false,
+  autoplay: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
 
-  $('.best-product-slider').slick({
-    infinite: true,
-    arrows: false,
-    dots: true,
-    // autoplay: true,
-    cssEase: 'ease-out',
-    slidesToShow: 4,
-    slidesToScroll: 4
-  });
+$('.best-product-slider').slick({
+  infinite: true,
+  arrows: false,
+  dots: true,
+  // autoplay: true,
+  cssEase: 'ease-out',
+  slidesToShow: 4,
+  slidesToScroll: 4
+});
 
-  $('.novelty-slider').slick({
-    infinite: true,
-    arrows: false,
-    autoplay: true,
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  });
+$('.novelty-slider').slick({
+  infinite: true,
+  arrows: false,
+  autoplay: true,
+  // dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1
+});
+
+$('.images-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.images-nav'
+});
+$('.images-nav').slick({
+  arrows: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.images-for',
+  // dots: true,
+  // centerMode: true,
+  variableWidth: true,
+  focusOnSelect: true
+});
